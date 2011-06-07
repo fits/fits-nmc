5cfed15d9af4f282ed359132f608f10541adda7e
fits <wadays_wozx@nifty.com>
2011-06-08 00:26:09 +0900

    Specs2 の追加と各種ライブラリのバージョンアップ

--- project/build/NewMusicCheckerProject.scala
+++ project/build/NewMusicCheckerProject.scala
@@ -1,16 +1,25 @@
 import sbt._
 
-class NewMusicCheckerProject(info: ProjectInfo) extends DefaultWebProject(info) {	//jetty-run を使用するために必要
-	val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.25" % "test"
-	//コンパイルに必要
-	val servletapi = "javax.servlet" % "servlet-api" % "2.5"
+class NewMusicCheckerProject(info: ProjectInfo) extends DefaultWebProject(info) {
+	val jettyVersion = "8.0.0.M3"
+	val scalatraVersion = "2.0.0-SNAPSHOT"
+	val specs2Version = "1.4"
 
-	//以降は Scalatra の設定
+	val jettyWebapp = "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "test"
+	//コンパイルに必要
+	val servletapi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+
+	//Scalatra のライブラリ設定
+	val scalatra = "org.scalatra" %% "scalatra" % scalatraVersion
+
+	//Specs2
+	val specs = "org.specs2" %% "specs2" % specs2Version % "test"
+	def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+	override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
+
 	//Scalatra のダウンロードサイトの設定
 	val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 	val sonatypeNexusReleases = "Sonatype Nexus Releases" at "https://oss.sonatype.org/content/repositories/releases"
-	//Scalatra のライブラリ設定
-	val scalatra = "org.scalatra" %% "scalatra" % "2.0.0-SNAPSHOT"
 
 	//GAE for Java SDK 
 	lazy val appengineSdkRoot = System.getenv("APPENGINE_SDK_HOME")
